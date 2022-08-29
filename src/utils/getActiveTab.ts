43e25ce999253fba82534e2906f94a160532f490
kevinejiro <kevinejiro@gmail.com>
2022-08-29 07:26:11 +0100

    update set active logic

--- src/utils/getActiveTab.ts
+++ src/utils/getActiveTab.ts
@@ -4,6 +4,30 @@ const getActiveTab = (location: string | string[] | undefined) => {
       return "Dashboard";
     case location?.includes("bulk-disburse"):
       return "Bulk Disburse";
+    case location?.includes("users"):
+      return "Users";
+    case location?.includes("history"):
+      return "History";
+    case location?.includes("notification"):
+      return "Notification";
+    case location?.includes("billers"):
+      return "Billers";
+    case location?.includes("product"):
+      return "Product";
+    case location?.includes("virtualAccount"):
+      return "Virtual account";
+    case location?.includes("terminal"):
+      return "Terminal Mgt";
+    case location?.includes("merchant"):
+      return "Merchant Mgt";
+    case location?.includes("audit-trail"):
+      return "Audit Trail";
+    case location?.includes("reconciliation"):
+      return "Reconciliation";
+    case location?.includes("roles-permissions"):
+      return "Roles & Permissions";
+    case location?.includes("warehouse"):
+      return "Warehouse Mgt";
     default:
       return "Dashboard";
   }
