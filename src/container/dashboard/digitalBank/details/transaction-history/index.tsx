fedee9583239bc5250ed3e9020b4bfa018b8c72e
Mohamed Fawzy <mohamed.f.elmorsi@gmail.com>
2022-10-21 15:42:50 +0200

    add transaction history tab with search filters

--- src/container/dashboard/digitalBank/details/transaction-history/index.tsx
+++ src/container/dashboard/digitalBank/details/transaction-history/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useQuery } from 'react-query'
+import { filterProps } from '../../../../../@types'
 import {
   FallBack,
   Filter,
@@ -10,17 +11,23 @@ import {
 } from '../../../../../components'
 import { filterValue } from '../../../../../data/filter-data'
 import { DBtransHeaderList } from '../../../../../data/table-headers'
-import { getResource } from '../../../../../utils/apiRequest'
+import { getNewFilterResource } from '../../../../../utils/apiRequest'
 
 const TransactionHistory = ({ userId }: { userId: string }) => {
-  const getTransactionsHistory = () => {
-    return getResource(`transactions?userId=${userId}`)
+  const [values, setValues] = useState(filterValue)
+
+  const getTransactionsHistory = (filterValue: filterProps) => {
+    return getNewFilterResource(
+      `transactions?userId=${userId}&`,
+      filterValue,
+      true
+    )
   }
 
-  const [, setValues] = useState(filterValue)
   const { isLoading, isError, data } = useQuery(
-    'user-transaction-history',
-    getTransactionsHistory
+    ['user-transaction-history', values],
+    () => getTransactionsHistory(values),
+    { keepPreviousData: true }
   )
 
   let component
@@ -63,7 +70,7 @@ const TransactionHistory = ({ userId }: { userId: string }) => {
               },
             ],
           }}
-          justifyContent={'flex-start'}
+          setFilterValues={setValues}
         />
 
         {component}
--- src/utils/apiRequest.ts
+++ src/utils/apiRequest.ts
@@ -18,13 +18,17 @@ export const getResource = async (pathUrl: string, withoutToken?: boolean) => {
 }
 export const getNewFilterResource = async (
   pathUrl: string,
-  filterValue: filterProps
+  filterValue: filterProps,
+  hasArg?: boolean
 ) => {
   const filterQuery = queryString.stringify(filterValue, {
     skipNull: true,
     skipEmptyString: true,
   })
-  const { data } = await axiosInstance.get(`/${pathUrl}?${filterQuery}`)
+  console.log(filterQuery)
+  const { data } = await axiosInstance.get(
+    `/${pathUrl}${hasArg ? '' : '?'}${filterQuery}`
+  )
   return data
 }
 export const getFilterResource = async (
