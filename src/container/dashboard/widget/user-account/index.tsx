15fb69992af0b06995a113b86e2a6b82901b85bf
OCHOWO <ochowo@Ochowo.local>
2022-10-28 13:50:40 +0100

    fix transaction detail  amount

--- src/container/dashboard/widget/user-account/index.tsx
+++ src/container/dashboard/widget/user-account/index.tsx
@@ -1,5 +1,6 @@
 import { Color } from '../../../../assets/theme'
 import { Stack, Text } from '../../../../components'
+import { capitalizeFirstLetterInSentence, removeHyphen } from '../../../../utils/formatValue'
 import { Card } from './styles/cards.styles'
 
 interface Props {
@@ -32,7 +33,7 @@ const UserAccount = ({ accountNumber, accountName, bank }: Props) => {
             Account Name
           </Text>
           <Text weight="600" color={Color.alerzoGrayishBlue2} size="1rem">
-            {accountName || '-'}
+            { capitalizeFirstLetterInSentence(accountName) || '-'}
           </Text>
         </Stack>
       </Stack>
@@ -45,7 +46,7 @@ const UserAccount = ({ accountNumber, accountName, bank }: Props) => {
           Bank name
         </Text>
         <Text weight="600" color={Color.alerzoGrayishBlue2} size="1rem">
-          {bank || '-'}
+          {removeHyphen(bank) || '-'}
         </Text>
       </Stack>
     </Card>
