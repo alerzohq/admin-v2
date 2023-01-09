3c7f99a06b41b9aed633a0ea672d99728629e684
OCHOWO <ochowo@Ochowo.local>
2023-01-09 21:08:30 +0100

    update kyc status

--- src/container/dashboard/kyc/details/index.tsx
+++ src/container/dashboard/kyc/details/index.tsx
@@ -14,14 +14,14 @@ import { KYCUser } from './KYCUser'
 const KYCDetailContainer = () => {
   const location = useLocation()
   const state = location.state as IStateProps
-
+const userId = state.userId;
   const getKYCLog = () => {
-    return getResource(`activity/logs?category=kyc&entityId=${state.userId}`)
+    return getResource(`activity/logs?category=kyc&entityId=${userId}`)
   }
 
   // pass stateId to useQuery func as params to avoid flicker
-  const { data, isLoading, isError, refetch, error } = useQuery(
-    'kyc-logs',
+  const { data } = useQuery(
+    ['kyc-logs', userId],
     () => getKYCLog()
   )
 
