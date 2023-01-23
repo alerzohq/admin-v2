9b1838412f371cd27b0f15dc6754c3066848c1eb
OCHOWO <ochowo@Ochowo.local>
2023-01-23 10:15:00 +0100

    terminal status update

--- src/container/dashboard/terminals/details/request-detail/request-details.tsx
+++ src/container/dashboard/terminals/details/request-detail/request-details.tsx
@@ -5,26 +5,33 @@ import POSRow from './pos-row'
 import { StatusModal } from './status-modal'
 import { terminalRequestHelper } from '../../../../../data/terminal-data'
 import Button from '../../../../../components/button'
+import { useLocation } from 'react-router-dom'
 
 export const RequestDetails = ({ terminalId, data }: ITerminalReqDetails) => {
   const [showStatusModal, setShowStatusModal] = useState(false)
-
+  interface Location {
+    businessId: string
+    status: { status: string }[]
+  }
+  const location = useLocation()
+  const state = location.state as Location
   return (
     <>
       <DetailsContent resolvedData={terminalRequestHelper(data)} />
       <Wrapper>
         <POSRow units={data?.data?.units} />
       </Wrapper>
-      {data?.data?.status !== 'delivered' && data?.data?.status !== 'rejected' && (
-        <Button
-          margin="2rem auto"
-          onClick={() => setShowStatusModal(true)}
-          className="add-button"
-          width="25%"
-        >
-          Update Terminal Status
-        </Button>
-      )}
+      {state?.status[state?.status?.length - 1]?.status !== 'delivered' &&
+        state?.status[state?.status?.length - 1]?.status !== 'rejected' && (
+          <Button
+            margin="2rem auto"
+            onClick={() => setShowStatusModal(true)}
+            className="add-button"
+            width="25%"
+          >
+            Update Terminal Status
+          </Button>
+        )}
       <StatusModal
         showModal={showStatusModal}
         setShowModal={() => setShowStatusModal(false)}
