f6c5cda90c41c052e316795ebbc62ad785aa4e50
OCHOWO <ochowo@Ochowo.local>
2023-01-24 13:43:07 +0100

    fixes

--- src/container/dashboard/terminals/details/request-detail/request-details.tsx
+++ src/container/dashboard/terminals/details/request-detail/request-details.tsx
@@ -21,8 +21,7 @@ export const RequestDetails = ({ terminalId, data }: ITerminalReqDetails) => {
       <Wrapper>
         <POSRow units={data?.data?.units} />
       </Wrapper>
-      {state?.status[state?.status?.length - 1]?.status !== 'delivered' &&
-        state?.status[state?.status?.length - 1]?.status !== 'rejected' && (
+      {state?.status[state?.status?.length - 1]?.status ===  'processing' && (
           <Button
             margin="2rem auto"
             onClick={() => setShowStatusModal(true)}
