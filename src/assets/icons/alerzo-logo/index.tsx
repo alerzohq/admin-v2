db2f50d53ac9db1aae1df5260d67849af37c5ca3
Ademola <ademola.sofolahan@alerzo.com>
2024-05-29 15:32:06 +0100

    increased size

--- src/assets/icons/alerzo-logo/index.tsx
+++ src/assets/icons/alerzo-logo/index.tsx
@@ -6,7 +6,7 @@ function AlerzoLogo({ color, height, width, className, onClick }: IconProps) {
     <svg
       className={className}
       width="400"
-      height="70"
+      height="100"
       viewBox="0 0 4248 2412"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
